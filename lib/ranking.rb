81cb81707250baecff375d5a69cf469d11f4b80a
Marcus Vinícius <marcus.f22@gmail.com>
2017-11-17 02:57:59 -0200

    Documentation of Ranking and TextInput.

--- lib/ranking.rb
+++ lib/ranking.rb
@@ -1,16 +1,22 @@
 class Ranking
   RANKING_PATH = "ranking.txt".freeze
 
+  # Initializes the Ranking object
   def initialize
     @file = File.new(RANKING_PATH, "a+")
     @scores = []
   end
 
+  # Write the username and score passed as argument into the ranking file.
+  # @param username, score [String, String]
+  # @return [Boolean] true or false depending on file closing.
   def save(username, score)
     @file.puts "#{username} - #{score}"
     @file.close
   end
 
+  # Return all ranks of ranking file sorted by score on desc order
+  # @return [Array<String>] ordered ranks
   def all_scores
     return @scores unless @scores.empty?
     File.open(RANKING_PATH, "r") do |f|
--- lib/text_input.rb
+++ lib/text_input.rb
@@ -1,14 +1,22 @@
 class TextInput < Gosu::TextInput
+  # Initialize TextInput object that inherits Gosu::TextInput
   def initialize
     super
   end
 
+  # Overriden method of Gosu::TextInput to filter if a string is valid or not.
+  # Invalid string are not captured. Valid strings are numbers and letters.
+  # @param text_in [String] the string written by the user
+  # @return [String] filtered string
   def filter(text_in)
     @last_char = text_in
     return if text.length > 2
     text_in.gsub(/[^a-zA-Z0-9]/, "").upcase
   end
 
+  # Returns true if the user has pressed the '=' character that represents
+  # end of input on this game.
+  # @return [Boolean] if user finished filling the input
   def finished?
     @last_char == "=" && !text.empty?
   end
